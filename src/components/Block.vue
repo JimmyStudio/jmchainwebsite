1bd705d9c7e2c3ddc6374cf8eb037899d1a99cfa
Jimmy <Jimmystudio@yeah.net>
2018-05-17 16:21:35 +0800

    get blocks

---
+++ src/components/Block.vue
@@ -0,0 +1,51 @@
+/**
+ * Created by Jimmy on 2018/5/17.
+ */
+<template>
+  <div class="block">
+    <div class="b-item b-item-1">{{block.number}}</div>
+    <div class="b-item b-item-2">{{block.timestamp}}</div>
+    <div class="b-item b-item-3">{{block.hash}}</div>
+    <div class="b-item b-item-4">{{block.transactions.length}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: Number,
+    block: Object
+  }
+}
+</script>
+
+<style scoped>
+  .block{
+    width: 1000px;
+    height: 60px;
+    margin: 10px 0;
+    /*border: 1px solid #5e5e5e;*/
+    /*background-color: #22252f;*/
+    font-size: 14px;
+    text-align: center;
+    line-height: 60px;
+    color: #5e5e5e;
+    font-weight: 500;
+    cursor: pointer;
+    -webkit-box-shadow:0 0 2px 2px #5e5e5e;
+    -moz-box-shadow:0 0 2px 2px #5e5e5e;
+    box-shadow:0 0 2px 2px #5e5e5e;
+  }
+  .b-item{
+    float: left;
+  }
+  .b-item-1, .b-item-2, .b-item-4{
+    width: 150px;
+  }
+  .b-item-3{
+    width: 550px;
+  }
+  .b-item-1{
+    /*color: #57a3fb;*/
+  }
+</style>
--- src/pages/Chain.vue
+++ src/pages/Chain.vue
@@ -5,39 +5,96 @@
   <div>
     <div class="main-content">
       <div class="sep"></div>
-      <div class="search"></div>
+      <div class="search">
+        <el-input
+          class="search-input"
+          placeholder="Hash"
+          suffix-icon="el-icon-search"
+          v-model="search">
+        </el-input>
+      </div>
       <div class="titles">
         <div class="item b-h">
-          <font-awesome-icon :icon="['fab','staylinked']"></font-awesome-icon> 区块高度
+          <font-awesome-icon :icon="['fas','th']"></font-awesome-icon> BlockNumber
         </div>
         <div class="item b-t">
-          <font-awesome-icon :icon="['far','clock']"></font-awesome-icon> 发布时间
+          <font-awesome-icon :icon="['far','clock']"></font-awesome-icon> Timestamp
         </div>
         <div class="item b-a">
-          <font-awesome-icon :icon="['fas','map-marker-alt']"></font-awesome-icon> 区块地址
+          <font-awesome-icon :icon="['fab','staylinked']"></font-awesome-icon> Hash
         </div>
         <div class="item b-n">
-          <font-awesome-icon :icon="['fas','chart-bar']"></font-awesome-icon> 条目数量
+          <font-awesome-icon :icon="['fas','chart-bar']"></font-awesome-icon> Transactions
         </div>
+      </div>
+      <div class="blocks">
+        <block v-for="(item , index) in blocks" :key="index" :block="item" :index="index"></block>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import block from '../components/Block.vue'
 export default {
+  components: {
+    block
+  },
+  data () {
+    return {
+      search: '',
+      blocks: [{}]
+    }
+  },
+  mounted: function () {
+    this.$http.get(this.domain + '/blocks')
+      .then((response) => {
+        if (response.data.err === '100') {
+          this.blocks = response.data.blocks
+        } else {
+          this.$message.error(response.data.message)
+        }
+      })
+      .catch(function (response) {
+        console.log(response)
+      })
+  }
 }
 </script>
 
 <style scoped>
+  .search >>> input{
+    border-color: #5e5e5e;
+    color: #5e5e5e;
+  }
+  .search >>> span{
+    color: #5e5e5e;
+    cursor: pointer;
+  }
+  .search{
+    width: 600px;
+    /*height: 60px;*/
+    /*background-color: blue;*/
+    /*background-color: #151823;*/
+    margin: 0 auto;
+    padding-top: 20px;
+    padding-bottom: 10px;
+  }
+  .blocks{
+    width: 1000px;
+    /*height: 500px;*/
+    background-color: white;
+    margin: 0 auto;
+  }
   .b-h, .b-t, .b-n{
-    width: 200px;
+    width: 150px;
   }
   .b-a{
-    width: 400px;
+    width: 550px;
   }
   .titles{
     width: 1000px;
+    height: 50px;
     margin: 0 auto;
   }
   .titles .item{
@@ -53,7 +110,8 @@ export default {
     width: 1240px;
     background-color: white;
     margin: 0 auto;
-    height: 500px;
+    padding-bottom: 20px;
+    /*height: 500px;*/
   }
   .sep{
     background-color: #f2f2f2;
